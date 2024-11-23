c61002f6351ddea7c94ce5b2087b43ee739f3d0d
Anse Min <anse.min@datasaur.ai>
2024-11-23 18:41:36 +0800

    Time: 10 ms (87.14%), Space: 11.7 MB (51.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,11 +1,14 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        result = []
-        i = 0
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                result.append(word1[i])
-            if i < len(word2):
-                result.append(word2[i])
-            i += 1
-        return ''.join(result)
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged=""
+        for i, j in zip(word1, word2):
+            merged+=i+j
+            
+        merged+=word1[len(word2):]+word2[len(word1):]
+        
+        return merged

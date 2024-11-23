9a84d69809afe4cd93152c2ad4afce007a44878b
Anse Min <anse.min@datasaur.ai>
2024-11-23 18:43:47 +0800

    Time: 17 ms (34.36%), Space: 11.8 MB (19.78%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        merged=""
-        for i, j in zip(word1, word2):
-            merged+=i+j
-            
-        merged+=word1[len(word2):]+word2[len(word1):]
+
+        merged=''
+        for i, j in zip(word1,word2):
+            merged+=i+j 
         
-        return merged
+        merged+=word1[len(word2):]+word2[len(word1):]
+        return merged 

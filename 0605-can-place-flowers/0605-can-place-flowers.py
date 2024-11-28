0805b7e482008f7f2305ad3da37e56a84f62cf2c
Anse Min <anse.min@datasaur.ai>
2024-11-28 16:57:59 +0800

    Time: 11 ms (51.88%), Space: 13 MB (7.22%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,12 +1,11 @@
-class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
         count=0
         for i in range(len(flowerbed)):
-            if flowerbed[i]==0:
-                right_side=(flowerbed[i-1]==0) or (i==0)
-                left_side=(i==len(flowerbed)-1) or (flowerbed[i+1]==0)
-                if right_side and left_side:
-                    flowerbed[i]=1
-                    count+=1
-        return count>=n
-        
+            if flowerbed[i]==0 and (i==0 or flowerbed[i-1]==0) and (i==len(flowerbed)-1 or flowerbed[i+1]==0):
+                flowerbed[i]=1
+                count+=1
+                if count>=n:
+                    return True
+        return count >=n
+            

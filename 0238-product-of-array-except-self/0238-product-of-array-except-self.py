f509275416deec9897f74cdd3296ce5a5d88d497
Anse Min <anse.min@datasaur.ai>
2024-12-23 01:49:40 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer=[1]*len(nums) #initializing 
+        #left
+        prefix=1
+        for i in range(len(nums)):
+            answer[i]=prefix
+            prefix*=nums[i]
+            print('step={}: prefix={}, answer={}'.format(i,prefix, answer))
+        #right
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            answer[i]*=suffix
+            suffix*=nums[i]
+            print('Step{}: suffix={}, answer={}'.format(i,suffix,answer))
+
+        return answer

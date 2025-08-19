b8be80b04ba91d8a83395301ddbdb61b11bed16a
Anse Min <ansemin1025@gmail.com>
2025-08-19 20:26:59 +0900

    Time: 19 ms (86.48%), Space: 23.4 MB (53.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,21 +1,15 @@
-class Solution(object):
+class Solution:
     def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        answer=[1]*len(nums) #initializing 
-        #left
-        prefix=1
-        for i in range(len(nums)):
-            answer[i]=prefix
-            prefix*=nums[i]
-            # print('step={}: prefix={}, answer={}'.format(i,prefix, answer))
-        #right
-        suffix=1
+        ans=[1]*len(nums)
+        right=1
         for i in range(len(nums)-1,-1,-1):
-            answer[i]*=suffix
-            suffix*=nums[i]
-            # print('Step{}: suffix={}, answer={}'.format(i,suffix,answer))
-
-        return answer
+            ans[i]=right
+            right*=nums[i]# num[i]=4
+            # print(f'Right : {right}')
+        # print(f'Result: {ans}')
+        left=1
+        for i in range(len(nums)):
+            ans[i]*=left
+            left*=nums[i]
+        return ans
+            

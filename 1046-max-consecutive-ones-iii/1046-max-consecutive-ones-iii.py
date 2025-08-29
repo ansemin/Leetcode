0cb66a999a1a78bf3912452255af2b044be32ea7
Anse Min <ansemin1025@gmail.com>
2025-08-29 13:38:53 +0900

    Time: 84 ms (30.34%), Space: 20.3 MB (31.79%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -1,25 +1,15 @@
-class Solution(object):
-    def longestOnes(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        original= [0]+nums+[0]
-        # print('orginal {}'.format(original))
-        zeros=[i for i, num in enumerate(original) if num==0] # define the 
-        # print('zeros {}'.format(zeros))
-        if len(zeros) <= k+2:
-            return len(nums)
-        current_value=0
-        max_value=0
-        for zp in range(len(zeros)-(k+1)):
-            left_boundary=zeros[zp]
-            right_boundary=zeros[zp+k+1]
-            # print('left boundary {}, right boundary {}'.format(left_boundary, right_boundary))
-            current_value = right_boundary-left_boundary-1
-            # print('current value is {}'.format(current_value))
-            if current_value>max_value:
-                max_value=current_value
-        # print('final value {}'.format(max_value))
-        return max_value 
+# Solution 2: Solving it using sliding window 
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left=0
+        curr=0
+        ans=0
+        for right in range(len(nums)):
+            if nums[right]==0:
+                curr+=1
+            while curr > k:
+                if nums[left]==0:
+                    curr-=1
+                left+=1
+            ans=max(ans,right-left+1)
+        return ans

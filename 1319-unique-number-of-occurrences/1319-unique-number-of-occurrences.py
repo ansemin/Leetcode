b6d59a47a612ee539cf4857042fb31fb0fb899ce
Anse Min <ansemin1025@gmail.com>
2025-09-06 23:22:59 +0900

    Time: 0 ms (100%), Space: 17.8 MB (90.32%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,14 +1,8 @@
+from collections import defaultdict
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        arrset=set(arr) # 1 2 3
-        result=dict() 
-        # build a dict
+        freq=defaultdict(int)
         for num in arr:
-            if num in arrset:
-                result[num]=result.get(num,0)+1
-        print(result) #new dict contain in this result 
-        values=set(list(result.values()))
-        keys=set(list(result.keys()))
-        print(f'values {values} keys {keys}')
-        return len(values)==len(keys) 
-
+            freq[num]=freq.get(num,0)+1
+        return len(set(freq.keys()))==len(set(freq.values()))
+        

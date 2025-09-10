5aa5e74c0276e9faa373e68dff47c2c3cbf5d9b8
Anse Min <ansemin1025@gmail.com>
2025-09-10 18:48:31 +0900

    Time: 110 ms (27.59%), Space: 22.1 MB (36.39%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -3,8 +3,12 @@ class Solution:
         # Get a column based list
         t=list(zip(*grid))
         count=0
+        # transform the grid into tuple 
+        for i,j in enumerate(grid):
+            grid[i]=tuple(j)
+
         for i in grid:
             for j in t:
-                if tuple(i)==j:
+                if i==j:
                     count+=1
         return count 

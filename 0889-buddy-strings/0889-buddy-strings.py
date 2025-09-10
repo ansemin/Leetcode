8da294d4ebe4ab3e7e9ee0686cdc02dc5e33251d
Anse Min <ansemin1025@gmail.com>
2025-09-10 14:47:09 +0900

    Time: 1 ms (56.89%), Space: 17.7 MB (94.46%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.py
+++ 0889-buddy-strings/0889-buddy-strings.py
@@ -1,20 +1,24 @@
+from collections import Counter 
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
+        #1st False case: Length is different
         if len(s)!=len(goal):
-            return False
+            return False 
+        if s==goal:
+            return len(set(s))<len(s)
+        #Track indices of difference
         diff=[]
-        for i in range(len(s)):
+        for i,j in enumerate(s):
             if s[i]!=goal[i]:
                 diff.append(i)
+        # False case, all the same indices 
         if not diff:
-            return len(set(s))<len(s)
-        print(diff) #[0,1]
-        if len(diff)==2:
-            i,j=diff
-            return s[i]==goal[j] and goal[i]==s[j]
-        return False 
-
-            
-         
+            return False
+        # False case, more than one difference
+        if len(diff)!=2:
+            return False  
+        i,j=diff
+        return s[i]==goal[j] and s[j]==goal[i]
 
 
+        

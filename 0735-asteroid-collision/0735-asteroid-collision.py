a5f79022529648160e2231f13841e9a88c5e58de
Anse Min <ansemin1025@gmail.com>
2025-09-11 21:19:37 +0900

    Time: 7 ms (59.39%), Space: 18.9 MB (37.5%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -6,14 +6,18 @@ class Solution:
             if a[i]>0:
                 result.append(a[i])
             elif a[i]<0:
-                while result and result[-1]>0 and abs(a[i])>abs(result[-1]):
+                while result and result[-1]>0 and abs(a[i])>result[-1]:
                     result.pop()
-                if result and result[-1]>0 and abs(a[i])==abs(result[-1]):
+                if result and result[-1]>0 and abs(a[i])==result[-1]:
                     result.pop()
                 elif not result or result[-1]<0:
                     result.append(a[i])
         return result
                 
                 
-            
+"""
+If positive add 
+If negative 
+    not Empty, Top element +ve, Ab(current element)>Ab(Top element)
+"""
                 

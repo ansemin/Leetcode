425733affd0d51c426cf790a9137a7ba5d300afc
Anse Min <ansemin1025@gmail.com>
2025-09-12 01:33:31 +0900

    Time: 7 ms (59.39%), Space: 19 MB (19.59%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,23 +1,16 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        a=asteroids #for convient shake
-        result=[] 
-        for i in range(len(a)):
-            if a[i]>0:
-                result.append(a[i])
-            elif a[i]<0:
-                while result and result[-1]>0 and abs(a[i])>result[-1]:
-                    result.pop()
-                if result and result[-1]>0 and abs(a[i])==result[-1]:
-                    result.pop()
-                elif not result or result[-1]<0:
-                    result.append(a[i])
+        result=[]
+        for ast in asteroids:
+            while result and result[-1]>0 and ast<0:
+                last=result.pop() #pops the element (can add it back or not)
+                if last>-ast: #if the result is positive then add it back
+                    result.append(last) #add it back
+                    break 
+                elif last==-ast:
+                    break #don't add anything 
+            else:
+                result.append(ast)
         return result
-                
-                
-"""
-If positive add 
-If negative 
-    not Empty, Top element +ve, Ab(current element)>Ab(Top element)
-"""
-                
+
+

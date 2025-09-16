8fd3419974638320e0e114c10a1cd3f4e960651e
Anse Min <ansemin1025@gmail.com>
2025-09-17 02:03:52 +0900

    Time: 0 ms (100%), Space: 17.8 MB (71.11%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def gcdOfStrings(self, str1, str2):
-        """
-        :type str1: str
-        :type str2: str
-        :rtype: str
-        """
-        len1, len2= len(str1),len(str2)
-        for i in range(min(len1,len2),0,-1):
-            if len1%i ==0 and len2%i==0:
-                base=str1[:i]
-                f1,f2=len1//len(base), len2//len(base)
-                if f1*base==str1 and f2*base==str2:
-                    return base
-        return ""
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2!=str2+str1:
+            return ""
+        from math import gcd
+        if len(str1)<len(str2):
+            return str1[:gcd(len(str1),len(str2))]
+        else:
+            return str2[:gcd(len(str1),len(str2))]

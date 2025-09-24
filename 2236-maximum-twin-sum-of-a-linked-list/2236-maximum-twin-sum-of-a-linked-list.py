3de5dde0c43871ed91f138fb6619d5b9203125af
Anse Min <ansemin1025@gmail.com>
2025-09-25 03:11:38 +0900

    Time: 48 ms (95.49%), Space: 41.3 MB (87.64%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -5,25 +5,37 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        fast=slow=head
-        while fast and fast.next:
-            fast=fast.next.next
-            slow=slow.next
-        # print(slow) # middle part 
-        prev=None
-        curr=slow
-        # print(slow)
-        # print(head)
-        while curr:
-            nextnode=curr.next #save
-            curr.next=prev #move curr.next
-            prev=curr # move prev
-            curr=nextnode
-        # print(prev)
-        head2=prev
-        maximum=0
-        while head2:
-            maximum=max(maximum, head2.val+head.val)
-            head=head.next
-            head2=head2.next
-        return maximum
+        # Find the midpoint of the list
+        dummy = head
+        slowpointer = dummy
+        fastpointer = dummy
+
+        while fastpointer != None:
+            slowpointer = slowpointer.next
+            fastpointer = fastpointer.next.next
+
+        # Now slowpointer marks the halfway point
+        dummy = head
+        current = dummy
+        prev = slowpointer
+        nextlocation = current.next
+        
+        while current != slowpointer:
+            current.next = prev
+            prev = current
+            current = nextlocation
+            nextlocation = current.next
+
+        dummy = prev
+        cursor1 = dummy
+        cursor2 = slowpointer
+        maximum_seen = 0
+
+        while cursor2 != None:
+            sum_of_pointers = cursor1.val + cursor2.val
+            if sum_of_pointers > maximum_seen:
+                maximum_seen = sum_of_pointers
+            cursor1 = cursor1.next
+            cursor2 = cursor2.next
+            
+        return maximum_seen

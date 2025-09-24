62c3946d499e1e5f61cc3b441d449b7239ca7dcd
Anse Min <ansemin1025@gmail.com>
2025-09-25 01:00:43 +0900

    Time: 89 ms (29.78%), Space: 47.7 MB (24.71%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -15,14 +15,25 @@ class Solution:
         # total length
         n=len(elements)
         maximum=0
-        for i in range(n):
-            pair=n-1-i
-            # print(f'i: {i}')
-            # print(f' pair is {pair}')
-            # print(type(pair))
-            total=elements[pair]+elements[i]
-            # print(total)
+        j=n-1 #initialize the end index
+        i=0
+        while i<j:
+            total=elements[i]+elements[j]
+            i+=1
+            j-=1
             maximum=max(total,maximum)
-        return maximum 
+        return maximum
+
+
+
+        # for i in range(n):
+        #     pair=n-1-i
+        #     # print(f'i: {i}')
+        #     # print(f' pair is {pair}')
+        #     # print(type(pair))
+        #     total=elements[pair]+elements[i]
+        #     # print(total)
+        #     maximum=max(total,maximum)
+        # return maximum 
 
 

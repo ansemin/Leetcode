dd41137e1a2a443ecd20cac34bbbf803a726afb8
Anse Min <ansemin1025@gmail.com>
2025-10-02 19:17:37 +0900

    Time: 0 ms (100%), Space: 18.3 MB (69.16%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -4,27 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.paths=0 #counter of the total number of valid path 
-        self.pathSums=defaultdict(int) # running sum we've seen so far
-        self.pathSums[0]=1 # if we come across targetSum in the root, we count this 
-
-        def dfs(r,currSum):
-            # base condition
-            if not r:
+        self.path=0
+        from collections import defaultdict
+        self.pathSum=defaultdict(int)
+        self.pathSum[0]=1
+        
+        def dfs(root,currSum):
+            if not root:
                 return 
-            currSum+=r.val #current running sum
-            self.paths+=self.pathSums[currSum-targetSum] # all the valid path from previous 
-            # if the key didn't exist, because we're using defaultdict, it would return 0 so it'd just be path+=0
-            self.pathSums[currSum]+=1 # we've now  seen sum 
-            #continuing the traversal
-            if r.right:
-                dfs(r.right,currSum)
-            if r.left:
-                dfs(r.left,currSum)
-            # After we process both nodes, we subtract the count
-            self.pathSums[currSum]-=1
+            currSum+=root.val
+            self.path+=self.pathSum[currSum-targetSum]
+            self.pathSum[currSum]+=1
+            if root.right:
+                dfs(root.right,currSum)
+            if root.left:
+                dfs(root.left,currSum)
+            self.pathSum[currSum]-=1
         dfs(root,0)
-        return self.paths
+        return self.path 

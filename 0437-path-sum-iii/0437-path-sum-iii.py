28e61d97d48824b1711fdc9b6d82a36719714f40
Anse Min <ansemin1025@gmail.com>
2025-10-02 18:57:24 +0900

    Time: 5 ms (71.61%), Space: 18.6 MB (20.49%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,20 +7,24 @@
 from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.paths=0
-        self.pathSums=defaultdict(int)
-        self.pathSums[0]=1
+        self.paths=0 #counter of the total number of valid path 
+        self.pathSums=defaultdict(int) # running sum we've seen so far
+        self.pathSums[0]=1 # if we come across targetSum in the root, we count this 
 
         def dfs(r,currSum):
+            # base condition
             if not r:
                 return 
-            currSum+=r.val
-            self.paths+=self.pathSums[currSum-targetSum]
-            self.pathSums[currSum]+=1
+            currSum+=r.val #current running sum
+            self.paths+=self.pathSums[currSum-targetSum] # all the valid path from previous 
+            # if the key didn't exist, because we're using defaultdict, it would return 0 so it'd just be path+=0
+            self.pathSums[currSum]+=1 # we've now  seen sum 
+            #continuing the traversal
             if r.right:
                 dfs(r.right,currSum)
             if r.left:
                 dfs(r.left,currSum)
+            # After we process both nodes, we subtract the count
             self.pathSums[currSum]-=1
         dfs(root,0)
         return self.paths

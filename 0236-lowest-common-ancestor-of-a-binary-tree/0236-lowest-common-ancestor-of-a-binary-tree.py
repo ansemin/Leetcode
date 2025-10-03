03628169a0cc852f7d3d8248076465efbe3b7da3
Anse Min <ansemin1025@gmail.com>
2025-10-04 00:17:33 +0900

    Time: 55 ms (51.28%), Space: 22.3 MB (10.03%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -9,38 +9,25 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.path_p=[]
         self.path_q=[]
-
-        def search(node, current_path):
+        def search(node, current_path=[]):
             if not node:
                 return 
-            # 1. Add the current node to the path
-            current_path.append(node) # p and q are nodes themselves, and we're not finding the node.val but the actual LCA node 
-
-            # 2. If we find p or q, take a snapshot (a copy) of the path
+            current_path.append(node)
             if node==p:
                 self.path_p=current_path.copy()
             if node==q:
                 self.path_q=current_path.copy()
-            
-            # 3. Explore children
             search(node.left,current_path)
             search(node.right,current_path)
-
-            # 4. Backtrack: Remove the current node from the path before returning
             current_path.pop()
-
-        # Start the traversal
         search(root,[])
+        # print(f'p path{self.path_p}')
+        # print(f'q path {self.path_q}')
 
-        # make it into set for efficient look up 
-        ancestor_p=set(self.path_p)
-
+        pnode=set(self.path_p)
         lca=None
-        # Iterate backwards through q's path. The first node we find that is also in p's path must be the deepest (lowest) one.
-        # forward search also work though (remove reversed() and break)
         for node in reversed(self.path_q):
-            if node in ancestor_p:
-                lca=node
-                break
-    
-        return lca 
+            if node in pnode:
+              lca=node  
+              break
+        return lca

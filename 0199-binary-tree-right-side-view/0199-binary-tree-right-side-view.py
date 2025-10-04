d4fede7db4dbf52b655e5c2d64893a92cc014932
Anse Min <ansemin1025@gmail.com>
2025-10-04 19:20:01 +0900

    Time: 0 ms (100%), Space: 17.9 MB (17.43%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,32 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rightSideView(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[int]
-        """
+from collections import deque 
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        
+        queue=deque([root])
         result=[]
-        queue=[root]
-
         while queue:
-            level_length=len(queue)
-
-            for i in range(level_length):
-                node=queue.pop(0)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                if i ==level_length-1:
-                    result.append(node.val)
-
-
+            level_size=len(queue)
+            for i in range(level_size):
+                current_node=queue.popleft()
+                if i==level_size-1:
+                    result.append(current_node.val)
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+                
         return result

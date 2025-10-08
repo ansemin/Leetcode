dc606e3602738aba15e5b718e1531c96a79fb3da
Anse Min <ansemin1025@gmail.com>
2025-10-08 20:55:35 +0900

    Time: 0 ms (100%), Space: 21.4 MB (32.51%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,24 +8,18 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-        # Traversing
         if key<root.val:
             root.left=self.deleteNode(root.left,key)
         elif key>root.val:
             root.right=self.deleteNode(root.right,key)
-        # found a match
         else:
-            # Either has 1 or 0 node
             if not root.left:
                 return root.right
             if not root.right:
                 return root.left
-            # have two nodes 
             temp=root.right
             while temp.left:
                 temp=temp.left
-            # set this smallest value to the root value
-            root.val=temp.val 
-            # now delete the smallest node from the tree
-            root.right=self.deleteNode(root.right,temp.val)
+            root.val=temp.val
+            root.right=self.deleteNode(root.right,root.val)
         return root

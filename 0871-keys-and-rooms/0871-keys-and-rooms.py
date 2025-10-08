e401a60d4516107ca57696d16fb28aee4482369c
Anse Min <ansemin1025@gmail.com>
2025-10-08 23:55:04 +0900

    Time: 0 ms (100%), Space: 18.2 MB (74.33%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,25 +1,12 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        nums_rooms=len(rooms)
-        # Track visited room 
         visited={0}
-        # initialize with the starting position of the room,0
         queue=deque([0])
         while queue:
-            # print('-------start------')
-            # print(f'queue is {queue}')
-            # print('------------------')
-            current_room=queue.popleft()
-            # print(f'current room is {current_room}')
-            keys_in_room=rooms[current_room]
-            # print(f'total keys are {keys_in_room}')
-            for key in keys_in_room:
-                # print(f'the key is {key}')
-                if key not in visited:
+            current=queue.popleft()
+            total_keys=rooms[current]
+            for key in total_keys:
+                if not key in visited:
                     visited.add(key)
                     queue.append(key)
-            # print(f'visited is {visited}')
-            # print('-----end of iteration----')
-        return len(visited)==nums_rooms
-
-                
+        return len(visited)==len(rooms)

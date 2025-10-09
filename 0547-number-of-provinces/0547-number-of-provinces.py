ac9216ed51d7becccb0f39854c1a261520e8cf41
Anse Min <ansemin1025@gmail.com>
2025-10-09 19:13:14 +0900

    Time: 3 ms (89.21%), Space: 18.7 MB (99.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,18 +1,24 @@
-from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n=len(isConnected)
-        visited=set()
+        from collections import deque 
+        # Initial the counter and visited memory
         province=0
-        for i in range(n):
+        visited=set()
+        for i in range(len(isConnected)):
             if i not in visited:
+                # Increase the increment
                 province+=1
+                # Set to queue for exploration
                 queue=deque([i])
+                # Add this to visited
                 visited.add(i)
                 while queue:
+                    # Extract the index of the current city
                     current_city=queue.popleft()
-                    for neighbor in range(n):
-                        if isConnected[current_city][neighbor]==1 and neighbor not in visited:
-                            visited.add(neighbor)
-                            queue.append(neighbor)
+                    for city in range(len(isConnected)):
+                        if isConnected[current_city][city]==1 and city not in visited:
+                            queue.append(city)
+                            visited.add(city)
+                
         return province
+

0b5bd2f47754988c558f136d25ddf66bef282684
Anse Min <ansemin1025@gmail.com>
2025-10-09 17:00:47 +0900

    Time: 0 ms (100%), Space: 18.1 MB (74.25%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,12 +1,15 @@
+from collections import deque
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited={0}
+        # Initialization
         queue=deque([0])
         while queue:
-            key_to_current_room=queue.popleft()
-            keys_to_next_room=rooms[key_to_current_room]
-            for key in keys_to_next_room:
+            current_key=queue.popleft()
+            next_keys=rooms[current_key]
+            for key in next_keys:
                 if not key in visited:
                     visited.add(key)
                     queue.append(key)
         return len(visited)==len(rooms)
+
